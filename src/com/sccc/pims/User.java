6a271998baece4df2b29cf68005aa9da079a1952
Administrator <Administrator@c303-021>
2016-12-13 11:51:36 +0800

    添加用户实体。

---
+++ src/com/sccc/pims/User.java
@@ -0,0 +1,9 @@
+package com.sccc.pims;
+
+public class User {
+    private String userName;
+    private String password;
+    private String name;
+    private char sex;
+    private String birth;
+}
